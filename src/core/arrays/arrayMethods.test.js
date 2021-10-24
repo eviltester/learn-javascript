e10d9648970830ec64bd6c983f11bca77e9830a6
alan <alan@compendiumdev.co.uk>
2021-10-24 11:42:00 +0100

    added more object and array examples

--- src/core/arrays/arrayMethods.test.js
+++ src/core/arrays/arrayMethods.test.js
@@ -26,3 +26,96 @@ describe('Arrays have methods to help, learn these well',()=>{
     });
 
 });
+
+describe('Setting full array values',()=>{
+
+
+
+    test('An array can be created with undefined values', ()=>{
+
+        const anArray = Array(5);
+
+        expect(anArray.length).toBe(5);
+        expect(anArray[0]).toBe(undefined);
+        expect(anArray[4]).toBe(undefined);
+
+    });
+
+    test('An array can be created and .fill(ed) with values', ()=>{
+
+        const anArray = Array(5).fill(1);
+        expect(anArray.length).toBe(5);
+        expect(anArray[0]).toBe(1);
+        expect(anArray[4]).toBe(1);
+
+    });
+
+    test('Care needs to be taken when filling with objects, otherwise they are the same object', ()=>{
+
+        const anArray = Array(5).fill({myNum: 1});
+
+        expect(anArray.length).toBe(5);
+
+        expect(anArray[0].myNum).toBe(1);
+        expect(anArray[4].myNum).toBe(1);
+
+        expect(anArray[0] === anArray[4]).toBe(true);
+
+        anArray[0].myNum=5;
+        expect(anArray[4].myNum).toBe(5);
+
+    });
+
+
+    test('Care needs to be taken when filling with objects, a new object is created once and added to the array', ()=>{
+
+        function prototypeObject(x){
+            this.myNum=x;
+        }
+
+        const anArray = Array(5).fill(new prototypeObject(1));
+
+        expect(anArray.length).toBe(5);
+
+        expect(anArray[0] === anArray[4]).toBe(true);
+
+    });
+
+
+    test('We can use .from to fill an array with new objects', ()=>{
+        
+        const anArray = Array.from(Array(5), () => {
+            return {myNum: 1};
+          });
+
+        expect(anArray.length).toBe(5);
+
+        expect(anArray[0].myNum).toBe(1);
+        expect(anArray[4].myNum).toBe(1);
+
+        expect(anArray[0] === anArray[4]).toBe(false);
+    });
+
+
+    test('We can not use .map on it\'s own to create filled arrays, because map ignores sparse undefined values', ()=>{
+        
+        const anArray = Array(5).map(() => {
+            return { myNum: 1 };
+          });
+
+        expect(anArray.length).toBe(5);
+        expect(anArray[0]).toBe(undefined);
+        
+    });
+
+    test('Spread creates a full array of undefined values, which can be used with map and is equivalent to .from', ()=>{
+        
+        const anArray = [...Array(5)].map(() => {
+            return { myNum: 1 };
+          });
+
+        expect(anArray.length).toBe(5);
+        expect(anArray[0].myNum).toBe(1);
+        
+    });
+});
--- src/core/objects/basicObjects.test.js
+++ src/core/objects/basicObjects.test.js
@@ -1,144 +1,204 @@
-test('an empty object can be created using braces', () => {
+describe('There are many different ways to create objects', ()=>{
+    test('an empty object can be created using braces', () => {
 
-    // this is called the 'literal' or 'initialization' notation
-    
-    var anObject={};
+        // this is called the 'literal' or 'initialization' notation
+        
+        var anObject={};
 
-    expect(typeof anObject).toBe("object");
-});
+        expect(typeof anObject).toBe("object");
+    });
 
-test('an object can have properties', () => {
+    test('an object can have properties', () => {
 
-    var anObject={a:2};
+        var anObject={a:2};
 
-    expect(anObject.a).toBe(2);
-});
+        expect(anObject.a).toBe(2);
+    });
 
-test('properties can be accessed with an index notation', () => {
+    test('properties can be accessed with an index notation', () => {
 
-    var anObject={a:2};
+        var anObject={a:2};
 
-    expect(anObject["a"]).toBe(2);
-});
+        expect(anObject["a"]).toBe(2);
+    });
 
-test('when a property does not exist, it is undefined', () => {
+    test('when a property does not exist, it is undefined', () => {
 
-    var anObject={};
+        var anObject={};
 
-    expect(anObject.a).toBe(undefined);
-});
+        expect(anObject.a).toBe(undefined);
+    });
 
-test('properties can be added to objects after creation', () => {
+    test('properties can be added to objects after creation', () => {
 
-    const anObject = {};
+        const anObject = {};
 
-    anObject.a = 1;
-    anObject["b"] = 2;
+        anObject.a = 1;
+        anObject["b"] = 2;
 
-    expect(anObject.a).toBe(1);
-    expect(anObject["a"]).toBe(1);
+        expect(anObject.a).toBe(1);
+        expect(anObject["a"]).toBe(1);
 
-    expect(anObject.b).toBe(2);
-    expect(anObject["b"]).toBe(2);
-});
+        expect(anObject.b).toBe(2);
+        expect(anObject["b"]).toBe(2);
+    });
 
-test('Properties can be removed from an object with delete', () => {
+    test('Properties can be removed from an object with delete', () => {
 
-    var anObject={a:2};
+        var anObject={a:2};
 
-    delete anObject.a;
+        delete anObject.a;
 
-    expect(anObject.a).toBe(undefined);
-});
-
-
-test('objects can be created using shorthand notation', () => {
-
-    const a=1, b=2, c=3;
-    const anObject = {a, b, c};
-
-    expect(anObject.a).toBe(1);
-    expect(anObject.b).toBe(2);
-    expect(anObject.c).toBe(3);
-
-    // this might be easier than 'key: value'
-    const anotherObject = {a: a, b: b, c: c};
-
-    expect(anotherObject.a).toBe(1);
-    expect(anotherObject.b).toBe(2);
-    expect(anotherObject.c).toBe(3);
-
-});
-
-test('objects can be created in different ways', () => {
-
-    const aNewObject= new Object();
-    const aLiteralObject = {};
-
-    expect(typeof aNewObject).toBe("object");
-    expect(typeof aLiteralObject).toBe("object");
-});
+        expect(anObject.a).toBe(undefined);
+    });
 
 
-test('Object.create requires a prototype', () => {
+    test('objects can be created using shorthand notation', () => {
 
-    const aPrototype = {a:1};
-    const aCreatedObject = Object.create(aPrototype);
+        const a=1, b=2, c=3;
+        const anObject = {a, b, c};
 
-    expect(typeof aCreatedObject).toBe("object");
-    expect(aCreatedObject.a).toBe(1);
-});
+        expect(anObject.a).toBe(1);
+        expect(anObject.b).toBe(2);
+        expect(anObject.c).toBe(3);
 
-test('functions can be used as objects', () => {
+        // this might be easier than 'key: value'
+        const anotherObject = {a: a, b: b, c: c};
 
-    function Position(x, y){
-        this.x=x;
-        this.y=y;
-    }
+        expect(anotherObject.a).toBe(1);
+        expect(anotherObject.b).toBe(2);
+        expect(anotherObject.c).toBe(3);
 
-    const xyPos = new Position(10,13);
+    });
 
-    expect(typeof xyPos).toBe("object");
-    expect(xyPos.x).toBe(10);
-});
+    test('objects can be created in different ways', () => {
+
+        const aNewObject= new Object();
+        const aLiteralObject = {};
+
+        expect(typeof aNewObject).toBe("object");
+        expect(typeof aLiteralObject).toBe("object");
+    });
 
 
-test('functions as objects can have methods declared using this with a function', () => {
+    test('Object.create requires a prototype', () => {
 
-    function Position(x, y){
-        this.x=x;
-        this.y=y;
+        const aPrototype = {a:1};
+        const aCreatedObject = Object.create(aPrototype);
 
-        this.asArray = function(){
+        expect(typeof aCreatedObject).toBe("object");
+        expect(aCreatedObject.a).toBe(1);
+    });
+
+    test('functions can be used as objects', () => {
+
+        function Position(x, y){
+            this.x=x;
+            this.y=y;
+        }
+
+        const xyPos = new Position(10,13);
+
+        expect(typeof xyPos).toBe("object");
+        expect(xyPos.x).toBe(10);
+    });
+
+
+    test('functions as objects can have methods declared using this with a function', () => {
+
+        function Position(x, y){
+            this.x=x;
+            this.y=y;
+
+            this.asArray = function(){
+                return [this.x,this.y];
+            }
+        }
+
+        const xyPos = new Position(10,13);
+
+        expect(typeof xyPos).toBe("object");
+        expect(xyPos.x).toBe(10);
+        expect(xyPos.asArray()[0]).toBe(10);
+    });
+
+    test('functions as objects can have methods declared using the prototype', () => {
+
+        function Position(x, y){
+            this.x=x;
+            this.y=y;
+        }
+
+        Position.prototype.asArray = function(){
             return [this.x,this.y];
         }
-    }
 
-    const xyPos = new Position(10,13);
+        const xyPos = new Position(10,13);
 
-    expect(typeof xyPos).toBe("object");
-    expect(xyPos.x).toBe(10);
-    expect(xyPos.asArray()[0]).toBe(10);
+        expect(typeof xyPos).toBe("object");
+        expect(xyPos.x).toBe(10);
+        expect(xyPos.asArray()[0]).toBe(10);
+    });
 });
 
-test('functions as objects can have methods declared using the prototype', () => {
 
-    function Position(x, y){
-        this.x=x;
-        this.y=y;
-    }
+describe('we can access and object\'s keys and values', ()=>{
+    test('Object.keys returns all the keys on the object as an array', () => {
 
-    Position.prototype.asArray = function(){
-        return [this.x,this.y];
-    }
+        function Position(x, y){
+            this.x=x;
+            this.y=y;
+        }
 
-    const xyPos = new Position(10,13);
+        const xyPos = new Position(10,13);
 
-    expect(typeof xyPos).toBe("object");
-    expect(xyPos.x).toBe(10);
-    expect(xyPos.asArray()[0]).toBe(10);
+        const xyPosKeys = Object.keys(xyPos);
+
+        expect(xyPosKeys.length).toBe(2);
+        expect(xyPosKeys).toContain("x");
+        expect(xyPosKeys).toContain("y");
+        
+    });
+
+    test('Object.values returns all the keys on the object as an array', () => {
+
+        function Position(x, y){
+            this.x=x;
+            this.y=y;
+        }
+
+        const xyPos = new Position(10,13);
+
+        const xyPosValues = Object.values(xyPos);
+
+        expect(xyPosValues.length).toBe(2);
+        expect(xyPosValues).toContain(10);
+        expect(xyPosValues).toContain(13);
+        
+    });
+
+    test('Object.entries returns all the [key,value] pairs on the object as an array', () => {
+
+        function Position(x, y){
+            this.x=x;
+            this.y=y;
+        }
+
+        const xyPos = new Position(10,13);
+
+        const xyPosValues = Object.entries(xyPos);
+
+        expect(xyPosValues.length).toBe(2);
+        xyPosValues.forEach((xyPosValue)=>{
+            key = xyPosValue[0];
+            expect(["x","y"]).toContain(key);
+            var expectedValue = key==="x" ? 10 : 13;
+            expect(xyPosValue[1]).toBe(expectedValue);    
+        });
+        
+    });
+
 });
-
 /*
 
 Reference:
